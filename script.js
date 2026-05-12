975af854cf920d1e2db7c63972c9afbb79986e58
Susant <susantpandit454@gmail.com>
2026-05-12 16:20:35 +1000

    Add JavaScript basics - variables, functions, conditionals

---
+++ script.js
@@ -0,0 +1,62 @@
+console.log("Hello from javascript!");
+
+let name = "Susant";
+let age = 20;
+let isLearning = true;
+
+console.log(name);
+console.log(age);
+console.log(isLearning);
+
+let firstname = "susant";
+let lastname = "pandit";
+let fullname = firstname + "" + lastname;
+console.log(fullname);
+
+const city = "sydney"
+console.log(city);
+
+let score = 0;
+score = 10;
+score = 20;
+console.log(score);
+
+
+function greet() {
+    console.log("hello susant");
+}
+
+greet();
+
+function namaste(PersonName) {
+    console.log("hello " + PersonName + "!");
+}
+
+namaste("pujan");
+namaste("roman");
+
+function add(a, b) {
+    return a + b;
+}
+ const result = add(5, 3);
+ console.log(result);
+
+ const agee= 16;
+ if (agee >= 18) {
+    console.log("you can vote");
+ } else {
+    console.log("you can't vote");
+ }
+
+
+ const scoree = 55;
+
+ if (scoree >= 90) {
+    console.log("grade A");
+ } else if (scoree >= 70) {
+    console.log("grade is B");
+ } else if (scoree >= 50) {
+    console.log("grade C"); 
+ } else {
+    console.log("grade F");
+ }
